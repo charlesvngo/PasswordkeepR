5b03d36838b1495b94ef34f218dd4aa77b3ace68
Charles Ngo <charlesngo37@gmail.com>
2022-05-07 13:40:57 -0700

    create helper function to render password cards

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,7 @@
 // Client facing scripts here
+
+// On initial load, render all the passwords in the database.
+$(() => {
+  $.get("/api/passwords/")
+    .then((response) => renderPasswordElement(response));
+});
---
+++ public/scripts/createPasswordElements.js
@@ -0,0 +1,35 @@
+
+// Function to create password cards from the database.
+
+const createPasswordElement = function(password) {
+  return `<article class="card p-1 shadow-lg rounded mb-5 mt-3 col-lg-8">
+  <div class="card-body">
+    <div class="d-flex align-items-center">
+      <h5 class="card-title">${password.website_url}</h5>
+      <h6 class="card-subtitle text-muted ml-3 pb-1">${password.category}</h6>
+    </div>
+    <div class="form-group row">
+      <div class="col-lg-6 d-flex pt-2">
+        <input type="text" readonly class="form-control-plaintext" value="${password.username}">
+        <button type="button" class="btn btn-outline-primary d-flex align-items-center"><i class="fa-solid fa-copy mr-2"></i> Copy</button>
+      </div>
+    </div>
+    <div class="form-group row">
+      <div class="col-lg-6 d-flex">
+        <input type="text" readonly class="form-control-plaintext" value="${password.password}">
+        <button type="button" class="btn btn-outline-primary d-flex align-items-center"><i class="fa-solid fa-copy mr-2"></i> Copy</button>
+      </div>
+    </div>
+    <div class="d-flex justify-content-between">
+      <button type="button" class="btn btn-outline-dark col-lg-2 col-sm-6 mr-1" data-toggle="modal" data-target=".bd-edit-modal-lg"> Edit</button>
+      <button type="button" class="btn btn-outline-danger col-lg-2 col-sm-6"><i class="fa-solid fa-trash"></i> Delete</button>
+    </div>
+  </div>
+</article>`;
+};
+
+const renderPasswordElement = function(response) {
+  for (const password of response.passwords) {
+    $("main").prepend(createPasswordElement(password));
+  }
+};
