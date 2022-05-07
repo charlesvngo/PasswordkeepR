dbbc9a61d440cea61f90cab9a8dd30e9c20a2ac0
Charles Ngo <charlesngo37@gmail.com>
2022-05-07 15:14:05 -0700

    add click handler to generate password

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -46,11 +46,31 @@ $(() => {
         $(this).prev().val(copyField).select();
         document.execCommand("copy");
       });
-    });
 
-  console.log(generatePassword(12, true, true, true, true));
-  console.log(generatePassword(12, true, true, true, true));
-  console.log(generatePassword(12, true, true, true, true));
-  console.log(generatePassword(12, true, true, true, true));
-  console.log(generatePassword(12, true, true, true, true));
+      $(".button-generate-password").click(function(event) {
+        // Obtain the selected length.
+        const $length = $(this).next().find("input");
+        const length = $length.val();
+
+        // From the DOM location of the length, check if lowerCase is true
+        const $lowerCase = $($length).next().children("input");
+        const lowerCase = $lowerCase.is(':checked');
+
+        // From the DOM location of the lowerCase, check if upperCase is true
+        const $upperCase = $($lowerCase).parent().next().children("input");
+        const upperCase = $upperCase.is(':checked');
+
+        // From the DOM location of the upperCase, check if num is true
+        const $num = $($upperCase).parent().next().children("input");
+        const num = $num.is(':checked');
+
+        // From the DOM location of the num, check if symbol is true
+        const $symbol = $($num).parent().next().children("input");
+        const symbol = $symbol.is(':checked');
+
+        const $inputField = $(this).prev().children("input");
+        $inputField.val(generatePassword(length, lowerCase, upperCase, num, symbol));
+      });
+
+    });
 });
