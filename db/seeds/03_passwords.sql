93b3df38befad8112db975276245625b09d1eccd
Cesar Santamaria <cesar-santamaria@outlook.com>
2022-05-10 13:34:31 -0500

    edit database urls to populate images

--- db/seeds/03_passwords.sql
+++ db/seeds/03_passwords.sql
@@ -9,8 +9,8 @@ VALUES (1, 'bob@vancerefridgeration.com', 'rOZv6kYpi9', 'vancerefridgeration.com
 (1, 'bob@vancerefridgeration.com', '_6a@*u1OQ(', 'bestbuy.com', 'shopping'),
 (1, 'bob@vancerefridgeration.com', 'O1b3r9c9eH', 'netflix.com', 'entertainment'),
 (1, 'about@vancerefridgeration.com', '_6a@*u1OQ(', 'amazon.com', 'shopping'),
-(2, 'jim@dundermifflin.com', 'u&c$(6qclf', 'dundermifflin.com', 'email'),
-(2, 'michael@dundermifflin.com', 'iNVHzsAMXr', 'dundermifflin.com', 'email'),
-(2, 'dwight@dundermifflin.com', 'z7X)BdY9$E', 'dundermifflin.com', 'email'),
+(2, 'jim@dundermifflin.com', 'u&c$(6qclf', 'dundermifflinpaper.com', 'email'),
+(2, 'michael@dundermifflin.com', 'iNVHzsAMXr', 'dundermifflinpaper.com', 'email'),
+(2, 'dwight@dundermifflin.com', 'z7X)BdY9$E', 'dundermifflinpaper.com', 'email'),
 (2, 'pam@dundermifflin.com', 'eW%OiY6F!T', 'staples.com', 'office'),
 (2, 'pam@dundermifflin.com', 'Ar_&9^%hOw', 'fedex.com', 'shipping');
