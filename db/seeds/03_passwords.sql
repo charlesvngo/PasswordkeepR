7e36e471b08a9ec7f5d182b92c44ffa9af0bb755
Brandon Felty <brcfelty@gmail.com>
2022-05-10 15:20:35 -0600

    adds seed data

--- db/seeds/03_passwords.sql
+++ db/seeds/03_passwords.sql
@@ -1,9 +1,9 @@
 -- passwords table seeds here
 
 INSERT INTO passwords (organization_id, username, password, website_url, category)
-VALUES (1, 'bob@vancerefridgeration.com', 'rOZv6kYpi9', 'vancerefridgeration.com', 'email'),
-(1, 'bee@vancerefridgeration.com', 'VMEu)dOJcN', 'vancerefridgeration.com', 'email'),
-(1, 'about@vancerefridgeration.com', '7yB5#Cp^#f', 'vancerefridgeration.com', 'email'),
+VALUES (1, 'bob@vancerefridgeration.com', 'rOZv6kYpi9', 'amazon.com', 'shopping'),
+(1, 'bee@vancerefridgeration.com', 'VMEu)dOJcN', 'td.com', 'finance'),
+(1, 'about@vancerefridgeration.com', '7yB5#Cp^#f', 'facebook.com', 'social'),
 (1, 'bob@vancerefridgeration.com', 'XqMBSp6dq', 'staples.com', 'office'),
 (1, 'bob@vancerefridgeration.com', '@(W$xXNT20', 'fedex.com', 'shipping'),
 (1, 'bob@vancerefridgeration.com', '_6a@*u1OQ(', 'bestbuy.com', 'shopping'),
@@ -13,4 +13,5 @@ VALUES (1, 'bob@vancerefridgeration.com', 'rOZv6kYpi9', 'vancerefridgeration.com
 (2, 'michael@dundermifflin.com', 'iNVHzsAMXr', 'dundermifflinpaper.com', 'email'),
 (2, 'dwight@dundermifflin.com', 'z7X)BdY9$E', 'dundermifflinpaper.com', 'email'),
 (2, 'pam@dundermifflin.com', 'eW%OiY6F!T', 'staples.com', 'office'),
-(2, 'pam@dundermifflin.com', 'Ar_&9^%hOw', 'fedex.com', 'shipping');
+(2, 'pam@dundermifflin.com', 'Ar_&9^%hOw', 'fedex.com', 'shipping'),
+(2, 'pam@dundermifflin.com', '%A@r5Aey', 'mondays.com', 'productivity');
