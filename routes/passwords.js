e80d8929f4151f4538a3739e3b9fe519c648b708
Charles Ngo <charlesngo37@gmail.com>
2022-05-10 11:50:18 -0700

    create a button to sort cards by category

--- routes/passwords.js
+++ routes/passwords.js
@@ -4,20 +4,41 @@ const router  = express.Router();
 
 module.exports = (db) => {
 
-    router.get("/:id", (req, res) => {
-      const id = req.params.id;
-      console.log("id: ", id);
-      db.query(`SELECT * FROM passwords WHERE id=$1;`, [id])
-        .then(data => {
-          const passwords = data.rows;
-          res.json({ passwords });
-        })
-        .catch(err => {
-          res
-            .status(500)
-            .json({ error: err.message });
-        });
-    });
+  router.get("/categories/:id", (req, res) => {
+    const user_id = req.cookies.user_id;
+    const category_id = req.params.id;
+
+    db.query(
+      `SELECT passwords.id AS id, organizations.id AS organization_id, username, password, website_url, category
+      FROM passwords
+      JOIN organizations ON organizations.id = passwords.organization_id
+      JOIN users ON organizations.id = users.organization_id
+      WHERE users.id=$1 AND category=$2;`, [user_id, category_id])
+      .then(data => {
+        const passwords = data.rows;
+        res.json({ passwords });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
+  router.get("/:id", (req, res) => {
+    const id = req.params.id;
+    console.log("id: ", id);
+    db.query(`SELECT * FROM passwords WHERE id=$1;`, [id])
+      .then(data => {
+        const passwords = data.rows;
+        res.json({ passwords });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
 
   router.get("/", (req, res) => {
     const user_id = req.cookies.user_id;
